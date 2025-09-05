5d6c02c79a56266ac83665981331f87ab178e9f2
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-09-05 16:02:50 +0300

    Vedio 01

--- Session 01/Program.cs
+++ Session 01/Program.cs
@@ -1,10 +1,59 @@
-﻿namespace Session_01
+﻿using Microsoft.VisualBasic;
+
+namespace Session_01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //-------------------------------------- Vedio 01 --------------------------------------
+            #region Implicitly Type Local Varibale [var, Dynamic]
+
+            #region Var
+            //string Data = "Ahmed";
+
+            //var Data = "Ahmed"; // Implicitly Typed
+            //                    // Complier will Detect Datatype Of Varriable at Compilition Time
+            //                    // Based on its intial Value
+
+            ////var x = null; // Invalid
+
+            //var Data; // Invalid
+
+            //Data = null;
+
+            //Data = "Mostafa";
+
+            ////Data = 10; // Invalid
+
+            #endregion
+
+            #region Dynamic
+            //dynamic Data = "Ali";
+            //                // CLR will Dectect Datatype Of Varriable At RunTime
+            //                // Based on it is Last Assign Value
+
+            //dynamic X;
+
+            //dynamic Y = null;
+
+            //Data = 10;
+            //Data = true;
+
+            //foreach (var item in collection)
+            //for (var i = 0;i < args.Length;i++) 
+
+            #endregion
+
+            //var x = null;
+            //dynamic x = null;
+
+            //Console.WriteLine(x);
+
+            #endregion
+
+
+
         }
     }
 }
