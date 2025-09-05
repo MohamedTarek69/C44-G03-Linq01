f3c86ce5b4adbb9dbe9cd0e73bab40db0cb41923
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-09-05 16:56:18 +0300

    Vedio 03

--- Session 01/Program.cs
+++ Session 01/Program.cs
@@ -69,6 +69,42 @@ namespace Session_01
 
             #endregion
 
+            //-------------------------------------- Vedio 03 --------------------------------------
+            #region Anonymous Type
+            //Employee employee = new Employee() { Id = 10, Name = "Mona", Salary = 8000 };
+
+            //object Emp01 = new { Id = 10, Name = "Mona", Salary = 8000 };
+            //Console.WriteLine(Emp01.Salary);
+
+            //int X = 5;
+
+            //Console.WriteLine(X.GetType().Name);
+
+            //var Emp03 = new { Id = 10, Name = "Mona", Salary = 8000 };
+
+            ////Console.WriteLine(Emp03.GetType().Name);// <>f__AnonymousType0`3
+            ////Console.WriteLine(Emp03.Salary);
+
+            //var Emp04 = new { Id = Emp03.Id, Name = Emp03.Name, Salary = 5000 }; // C# 09
+            //var Emp05 = Emp03 with { Salary = 2000 }; // C# 10
+
+            ////Console.WriteLine($"Emp 03 = {Emp03}");
+            ////Console.WriteLine($"Emp 04 = {Emp04}");
+            ////Console.WriteLine($"Emp 05 = {Emp05}");
+            ////Console.WriteLine($"Emp 03 = {Emp03.GetType().Name}");// <>f__AnonymousType0`3
+            ////Console.WriteLine($"Emp 04 = {Emp04.GetType().Name}");// <>f__AnonymousType0`3
+            ////Console.WriteLine($"Emp 05 = {Emp05.GetType().Name}");// <>f__AnonymousType0`3
+
+            //// The Same Anonymous Type as long as:
+            //// 1. Same Properties Name [Case Senstive]
+            //// 2. Same Properties Order
+
+            //var Emp06 = new { id = 10, Name = "Mona", Salary = 8000 };
+            //var Emp07 = new { Name = "Mona", id = 10, Salary = 8000 };
+            //Console.WriteLine($"Emp 06 = {Emp06.GetType().Name}");// <>f__AnonymousType1`3
+            //Console.WriteLine($"Emp 07 = {Emp07.GetType().Name}");// <>f__AnonymousType2`3
+
+            #endregion
 
 
         }
