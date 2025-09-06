d90ea7c8ebd3be0ff29f299e912caaba72263a81
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-09-06 20:58:49 +0300

    LINQ - Element Operators

--- Session 01/Program.cs
+++ Session 01/Program.cs
@@ -1,4 +1,5 @@
-﻿using static Session_01.ListGenerator;
+﻿using System.Collections.Generic;
+using static Session_01.ListGenerator;
 namespace Session_01
 {
     internal class Program
@@ -42,6 +43,37 @@ namespace Session_01
             #endregion
 
             #endregion
+
+            #region LINQ - Element Operators
+
+            #region Q1
+            //1.Get first Product out of Stock
+
+            //var Result = ProductList.FirstOrDefault(p => p.UnitsInStock == 0);
+            //Console.WriteLine(Result);
+
+            #endregion
+
+            #region Q2
+            //2. Return the first product whose Price > 1000, unless there is no match, in which case null is returned.
+
+            //var Result = ProductList.FirstOrDefault(p => p.UnitPrice > 1000);
+            //Console.WriteLine(Result is not null ? Result :"Not Fount");
+
+            #endregion
+
+            #region Q3
+            //Retrieve the second number greater than 5
+
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var Result = Arr.Where(n => n > 5).Skip(1).FirstOrDefault();
+            //Console.WriteLine(Result);
+
+            #endregion
+
+            #endregion
+
+
         }
     }
 }
